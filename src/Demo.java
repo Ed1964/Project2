2c58eaa8d2f3f0d3755d7b974f10fb62490a3676
Zvyagintsev Sergey <professor214@gmail.com>
2020-10-25 21:36:41 +0300

    second commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,7 @@
 public class Demo {
     public static void main(String args[])  {
     for(int x= 0; x<10; x++){
-        for (int y =0; y<10; y++){
+        for (int y =0; y<100; y++){
             System.out.println(y * x+" ");
         }
         System.out.print("");
