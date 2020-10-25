f9124db27c539ca857eafeee04442d693b3ea6a3
Zvyagintsev Sergey <professor214@gmail.com>
2020-10-25 21:21:49 +0300

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by nb on 01.02.2015.
+ */
+public class Demo {
+    public static void main(String args[])  {
+    for(int x= 0; x<10; x++){
+        for (int y =0; y<10; y++){
+            System.out.println(y * x+" ");
+        }
+        System.out.print("");
+    }
+    }
+}
